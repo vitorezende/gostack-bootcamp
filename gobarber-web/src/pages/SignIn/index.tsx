d6cef4327429da8afc8e017dd5fdd2eccdf4720b
Vitore Rezende <vitorezende@gmail.com>
2020-11-21 22:16:33 -0400

    Isolando componentes

--- gobarber-web/src/pages/SignIn/index.tsx
+++ gobarber-web/src/pages/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -16,9 +16,14 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu login</h1>
 
-        <Input name="email" placeholder="E-mail" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-        <Input name="password" type="password" placeholder="Password" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Password"
+        />
 
         <Button type="submit">Entrar</Button>
 
